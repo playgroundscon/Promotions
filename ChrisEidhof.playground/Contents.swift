53a484721aca2abf80456f4dba37116a98240d02
Andyy Hope <andyyhope@users.noreply.github.com>
2016-10-27 22:11:06 +1100

    ADDED: Chris Eidhof promotional offer

---
+++ ChrisEidhof.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playgrounds - Swift and Apple Developers Conference
+//: Melbourne, February 23 & 24
+//: Website: http://www.playgroundscon.com
+//: Twitter: @playgroundscon
+//: Contact: hello@playgroundscon.com
+
+import Foundation
+import UIKit
+
+
+// MARK: - Availability
+
+enum Available {
+    case now
+    case tomorrow
+}
+
+
+// MARK: - Promo Model
+
+struct Promo {
+    let code: String
+    let discount: Double
+    let limit: Int
+    let available: Available
+    
+    func claim() {
+        let url = URL(string: "http://www.playgroundscon.com/tickets")!
+        let options: [String : Any] = ["promo": self]
+        
+        UIApplication.shared.open(url, options: options, completionHandler: nil)
+        print("Thank you for your support ❤️")
+    }
+}
+
+
+// MARK: - Promotional Offer
+
+let chrisEidhofPromo = Promo(
+    code: "CHRISEIDHOF",
+    discount: 50,
+    limit: 1,
+    available: .now)
+
+chrisEidhofPromo.claim()
+
+
+
